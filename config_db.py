41eb44000a7b3e6b534fff326a812482130cc6fa
IsaacMur <murilloabraham976@gmail.com>
2024-12-14 17:35:47 -0600

    Cambios del sabado del 14-12-2024

--- config_db.py
+++ config_db.py
@@ -1,8 +1,8 @@
 from flask_mysqldb import MySQL  # type: ignore
 
 def init_db(app):
-    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_HOST'] = 'dpg-ctf1bpbtq21c73bqfub0-a'
     app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'admin'
+    app.config['MYSQL_PASSWORD'] = 'JaoCzQoZye5wHW8O1Qpu1LcS61JIFmFD'
     app.config['MYSQL_DB'] = 'famco'
     return MySQL(app)
